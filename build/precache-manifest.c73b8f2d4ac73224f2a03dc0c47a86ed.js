b78e64484fd0a709f5042870ce43ac9d8bca74ac
ashut0shyadav <76781784+ashut0shyadav@users.noreply.github.com>
2022-06-30 02:30:32 +0530

    Add files via upload

---
+++ build/precache-manifest.c73b8f2d4ac73224f2a03dc0c47a86ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e7431a982991b269efb",
+    "url": "/static/css/main.b5b89743.chunk.css"
+  },
+  {
+    "revision": "4e7431a982991b269efb",
+    "url": "/static/js/main.4e7431a9.chunk.js"
+  },
+  {
+    "revision": "4ed58c52e6f368d5f236",
+    "url": "/static/css/1.6ab639ca.chunk.css"
+  },
+  {
+    "revision": "4ed58c52e6f368d5f236",
+    "url": "/static/js/1.4ed58c52.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "487c3404aa1410ebdfb88ec25076f4d9",
+    "url": "/index.html"
+  }
+];
